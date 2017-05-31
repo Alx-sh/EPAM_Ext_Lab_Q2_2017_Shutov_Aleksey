3f43d428cde21f18bd7dd37d5ae1e753e92687da
Alx-sh <ash96@bk.ru>
2017-05-31 19:34:13 +0400

    Home work #1
    Task01, Task02 added.

---
+++ HWT_01/Task01/Task01/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+
+namespace Task01
+{
+    ///<summary>
+    /// Написать консольное приложение, которое проверяет принадлежность точки заштрихованной области.
+    /// Пользователь вводит координаты точки (x; y) и выбирает букву графика(a-к). В консоли должно высветиться сообщение: «Точка[(x; y)] принадлежит фигуре[г]».
+    ///</summary>
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            string letter;
+
+            while (true)
+            {
+                bool isDouble = false;
+                bool? belong = false;
+
+                Console.Clear();
+                Console.WriteLine("Введите координаты точки (x,y):");
+                while (true)
+                {
+                    Console.Write("x = ");
+                    isDouble = double.TryParse(Console.ReadLine(), out x);
+                    if (isDouble == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верный формат входной строки! Введите действительное или целое число.");
+                    }
+                }
+                while (true)
+                {
+                    Console.Write("y = ");
+                    isDouble = double.TryParse(Console.ReadLine(), out y);
+                    if (isDouble == true)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верный формат входной строки! Введите действительное или целое число.");
+                    }
+                }
+
+                Console.WriteLine("Выберите букву графика (а - к) или 0 для выхода:");
+                letter = Console.ReadLine();
+                switch (letter)
+                {
+                    case "0":
+                        {
+                            return;
+                        }
+                    case "а":
+                        {
+                            if (Math.Pow(x, 2) + Math.Pow(y, 2) <= Math.Pow(1, 2))
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "б":
+                        {
+                            if ((Math.Pow(x, 2) + Math.Pow(y, 2) <= Math.Pow(1, 2)) && (Math.Pow(x, 2) + Math.Pow(y, 2) >= Math.Pow(0.5, 2)))
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "в":
+                        {
+                            if (Math.Abs(x) <= 1 && Math.Abs(y) <= 1)
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "г":
+                        {
+                            if (Math.Abs(x) + Math.Abs(y) <= 1)
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "д":
+                        {
+                            if (Math.Abs(x) * 2 + Math.Abs(y) <= 1)
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "е":
+                        {
+                            if (x <= 0 && x >= -2 && (Math.Abs(x) * 0.5 + Math.Abs(y) <= 1))
+                            {
+                                belong = true;
+                            }
+                            else
+                            {
+                                if (x > 0 && x <= 1 && (Math.Pow(x, 2) + Math.Pow(y, 2) <= 1))
+                                {
+                                    belong = true;
+                                }
+                            }
+                            break;
+                        }
+                    case "ж":
+                        {
+                            if (y >= -1 && y <= 2 && (Math.Abs(x) * 2 + y <= 2))
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "з":
+                        {
+                            if (y >= -2 && y <= 0 && Math.Abs(x) <= 1)
+                            {
+                                belong = true;
+                            }
+                            else
+                            {
+                                if (y > 0 && y <= 1 && Math.Abs(x) >= y)
+                                {
+                                    belong = true;
+                                }
+                            }                    
+                            break;
+                        }
+                    case "и":
+                        {
+                            if ((y * 3 - x >= -1) && ((y <= 0 && x >= 0) || (y / 2  - x <= 1.5) && (-1 * x >= y)))
+                            {
+                                belong = true;
+                            }
+                            break;
+                        }
+                    case "к":
+                        {
+                            if (y >= 1)
+                            {
+                                belong = true;
+                            }
+                            else
+                            {
+                                if (y >= 0 && (Math.Abs(x) <= y))
+                                {
+                                    belong = true;
+                                }
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("График не найден! Повторите ввод.");
+                            belong = null;
+                            break;
+                        }
+                }
+                if (belong == true)
+                {
+                    Console.WriteLine("Точка ({0};{1}) принадлежит фигуре [{2}].", x, y, letter);
+                }
+                else
+                {
+                    if (belong == false)
+                    {
+                        Console.WriteLine("Точка ({0};{1}) не принадлежит фигуре [{2}].", x, y, letter);
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ HWT_01/Task02/Task02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Task02
+{
+    /// <summary>
+    /// Дано действительное число h. Выяснить, имеет ли уравнение ax^2 + bx + c = 0 действительное корни, 
+    /// если определены a, b, c, и если существуют, то найти их.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double D;
+            double h;
+            double x1;
+            double x2;
+            bool isDouble;
+
+            Console.WriteLine("Введите действительное число h:");
+            while (true)
+            {
+                Console.Write("h = ");
+                isDouble = double.TryParse(Console.ReadLine(), out h);
+                if (isDouble == true)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Не верный формат входной строки! Введите действительное или целое число.");
+                }
+            }
+
+            a = Math.Sqrt(((Math.Abs(Math.Sin(8 * h))) + 17) / (Math.Pow(1 - Math.Sin(4 * h) * Math.Cos(Math.Pow(h, 2) + 18), 2)));
+            b = 1 - Math.Sqrt(3 / (3 + Math.Abs(Math.Tan(a * Math.Pow(h, 2)) - Math.Sin(a * h))));
+            c = a * Math.Pow(h, 2) * Math.Sin(b * h) + (b * Math.Pow(h, 3)) * Math.Cos(a * h);
+            D = Math.Pow(b, 2) - 4 * a * c;
+
+            Console.WriteLine("a = {0}\nb = {1}\nc = {2}\nD = {3}", a, b, c, D);
+
+            if (D >= 0)
+            {
+                x1 = (-b + Math.Sqrt(D)) / (2 * a);
+                x2 = (-b - Math.Sqrt(D)) / (2 * a);
+                if (x1 == x2)
+                {
+                    Console.WriteLine("Корень уравнения:\nx1 = {0}", x1);
+                }
+                else
+                {
+                    Console.WriteLine("Корни уравнения:\nx1 = {0}\nx2 = {1}", x1, x2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Корней нет");
+            }
+            Console.ReadKey();
+        }
+    }
+}
