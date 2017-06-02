61724be2e44224570c8bd2500720fef759f28a93
Alx-sh <ash96@bk.ru>
2017-06-02 21:20:52 +0400

    HWT_04
    HWT_04 выполнено.

---
+++ HWT_04/Task01/Program.cs
@@ -0,0 +1,78 @@
+﻿/// <summary>
+/// Написать программу, которая определяет среднюю длину слова во введенной текстовой строке. 
+/// Учесть, что символы пунктуации на длину слов влиять не должны. 
+/// Регулярные выражения не использовать. 
+/// И не пытайтесь прописать все ручками. 
+/// Используйте стандартные методы класса String.
+/// </summary>
+namespace Task01
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        /// <summary>
+        /// Очищаем строку от лишних знаков пунктуации и пробелов.
+        /// </summary>
+        /// <param name="str"> Входная строка. </param>
+        static void ClearString (ref string str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char ch in str)
+            {
+                if (!char.IsPunctuation(ch))
+                {
+                    sb.Append(ch);
+                }
+            }
+
+            str = sb.ToString();
+
+            while (str.StartsWith(" "))
+            {
+                str = str.TrimStart(' ');
+            }
+
+            while (str.EndsWith(" "))
+            {
+                str = str.TrimEnd(' ');
+            }
+
+            while (str.Contains("  "))
+            {
+                str = str.Replace("  ", " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string str;
+            int sum = 0;
+            int countWord = 1;
+            int average;
+           
+            Console.WriteLine("Введите текстовую строку:");
+            str = Console.ReadLine();
+
+            ClearString(ref str);
+
+            foreach (char ch in str)
+            {
+                sum += (char.IsLetterOrDigit(ch) && !char.IsPunctuation(ch)) ? 1 : 0;
+                countWord += (char.IsSeparator(ch) ? 1 : 0);
+            }
+
+            Console.WriteLine("Сумма всех букв в строке = {0}\nКоличество слов = {1}", sum, countWord);
+
+            average = sum / countWord;
+
+            Console.WriteLine("Средняя длина слова в веденной строке - {0} символов", average);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HWT_04/Task02/Program.cs
@@ -0,0 +1,42 @@
+﻿/// <summary>
+/// Написать программу, которая удваивает в первой введенной строки все символы, принадлежащие второй введенной строке.
+/// Пример:
+/// Введите первую строку: написать программу, которая
+/// Введите вторую строку: описание
+/// Результирующая строка: ннааппииссаать ппроограамму, коотоораая
+/// </summary>
+namespace Task02
+{
+    using System;
+    using System.Text;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+ 
+            string strFirst;
+            string strSecond;
+            StringBuilder result = new StringBuilder();
+
+            Console.Write("Введите первую строку: ");
+            strFirst = Console.ReadLine();
+            Console.Write("Введите вторую строку: ");
+            strSecond = Console.ReadLine();
+
+            foreach (char ch in strFirst)
+            {
+                result.Append(ch);
+                if (strSecond.Contains(ch.ToString()))
+                {
+                    result.Append(ch);
+                }  
+            }
+
+            Console.WriteLine("Результирующая строка: {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
